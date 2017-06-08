67cb495ba5eb40b1c9a6a5e5a48a9e4d586b1454
Xavier Sánchez Atienza <xaviersanchez@dorna.com>
2017-06-08 17:00:40 +0200

    First graphql tutorial chapter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-// import { graphql, buildSchema } from 'graphql';
+import { graphql, buildSchema } from 'graphql';
+
+// Construc a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => 'Hello world',
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
