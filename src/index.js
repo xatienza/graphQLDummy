4375f35504175bf77b6b53ab4db62f1fc9c81177
Xavier Sánchez Atienza <xaviersanchez@dorna.com>
2017-06-08 18:03:48 +0200

    Mutations and Input Types - Part II

--- src/index.js
+++ src/index.js
@@ -1,26 +1,58 @@
 import { buildSchema } from 'graphql';
+import crypto from 'crypto';
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
+import Message from './models/Message';
+
 
 // Construc a schema, using GraphQL schema language
 const schema = buildSchema(`
-  type Mutation {
-    setMessage(message: String): String
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
   }
 
   type Query {
-    getMessage: String
+    getMessage: String    
+  }
+  
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
   }`);
 
 const fakeDatabase = {};
 
 // The root provides a resolver function for each API endpoint
 const root = {
-  setMessage: ({ message }) => {
-    fakeDatabase.message = message;
-    return message;
+  createMessage: ({ input }) => {
+    // Create a random id for our "database".
+    const id = crypto.randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
   },
-  getMessage: () => fakeDatabase.message,
+  getMessage: ({ id }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error(`no message exists with id ${id}`);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  updateMessage: ({ id, input }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error(`no message exists with id ${id}`);
+    }
+
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
 };
 
 const app = express();
---
+++ src/models/Message.js
@@ -0,0 +1,9 @@
+class Message {
+  constructor(id, { content, author }) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+export default Message;
