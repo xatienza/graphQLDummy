9f8cfe2ba22e4e7c413e0407a56b1418e4b66590
Xavier Sánchez Atienza <xaviersanchez@dorna.com>
2017-06-08 17:48:14 +0200

    Mutations and Input Types Part I

--- src/index.js
+++ src/index.js
@@ -1,39 +1,26 @@
 import { buildSchema } from 'graphql';
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import RandomDie from './models/RandomDie';
 
 // Construc a schema, using GraphQL schema language
 const schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
+  type Mutation {
+    setMessage(message: String): String
   }
 
   type Query {
-    hello: String
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
-    rollDice(numDice: Int!, numSides: Int): [Int]
-    getDie(numSides: Int): RandomDie
+    getMessage: String
   }`);
+
+const fakeDatabase = {};
 
 // The root provides a resolver function for each API endpoint
 const root = {
-  hello: () => 'Hello world',
-  quoteOfTheDay: () => (Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'),
-  random: () => Math.random(),
-  rollThreeDice: () => [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6)),
-  rollDice: ({ numDice, numSides }) => {
-    const output = [];
-    for (let i = 0; i < numDice; i += 1) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
-    }
-    return output;
+  setMessage: ({ message }) => {
+    fakeDatabase.message = message;
+    return message;
   },
-  getDie: ({ numSides }) => new RandomDie(numSides || 6),
+  getMessage: () => fakeDatabase.message,
 };
 
 const app = express();
